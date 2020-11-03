3ba757c7a1eb8359c977cbcb29ba4ba6c5fa331d
Luis1505 <luis.montanez-vazquez@patelhs.org>
2020-11-03 18:35:22 +0000

    did it

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-   int[ ] scores = {-20, -15, 2, 8, 16, 33};
+   int[ ] scores = {78, 89, 23, 88, 90, 93, 69, 100, 81,43};
+  
+   for(int i = 9;i >=0 ; i--){
+  //only print the number if it is even
+   if(scores [i] % 2 == 0){
+     System.out.println("This number is even " + scores [i]);
+   }
+   //adding a else statement if and odd number
+   else{
+     System.out.println("This number is odd " + scores [1]);
+   }
   }
+ }
 }
